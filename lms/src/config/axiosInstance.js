ed7ce132d36ce95cdb4ba1d27c30a7d7de21581d
Shrivarsha <123290462+shrivarshapoojari@users.noreply.github.com>
2023-11-11 20:59:50 +0530

    axios instance added

---
+++ lms/src/config/axiosInstance.js
@@ -0,0 +1,8 @@
+ import axios from "axios"
+const BASE_URL="http://localhost:5014/api/v1"
+
+ const axiosInstance=axios.create()
+axiosInstance.defaults.baseURL=BASE_URL;
+axiosInstance.defaults.timeout=3500;
+
+ export default axiosInstance;
