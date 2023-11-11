69264119668eebf514600afa557c1d9b04fbaae2
Shrivarsha <123290462+shrivarshapoojari@users.noreply.github.com>
2023-11-11 20:54:18 +0530

    Redux added

--- lms/src/index.js
+++ lms/src/index.js
@@ -4,12 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Toaster } from 'react-hot-toast';
-
+import store from './redux/store';
+import {Provider} from 'react-redux'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
     <Toaster/>
+    </Provider>
   </React.StrictMode>
 );
 
