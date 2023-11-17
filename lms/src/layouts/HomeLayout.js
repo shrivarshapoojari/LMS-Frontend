3c1db624be80deb5a61ce52c3154b25e94601bca
Shrivarsha <123290462+shrivarshapoojari@users.noreply.github.com>
2023-11-17 21:03:22 +0530

    Added Side action buttons to login logout etc

--- lms/src/layouts/HomeLayout.js
+++ lms/src/layouts/HomeLayout.js
@@ -1,12 +1,20 @@
 import React from 'react'
  
 import {FiMenu} from 'react-icons/fi'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import {AiFillCloseCircle} from 'react-icons/ai'
 import Footer from '../components/Footer'
+import { useDispatch, useSelector }from 'react-redux'
+import authSlice from '../redux/slices/authSlice'
 const HomeLayout = ({children}) => {
 
+const dispatch=useDispatch();
 
+
+const navigate=useNavigate()
+
+const isLoggedIn=useSelector((state)=>state?.auth?.isLoggedIn)
+const role=useSelector((state)=>state?.auth?.role);
     function changeWidth()
     {
         const drawerSide=document.getElementsByClassName("drawer-side");
@@ -24,9 +32,18 @@ const HomeLayout = ({children}) => {
         drawerSide[0].style.width='auto';
     }
 
+
+    function onLogout(e)
+    {
+        e.preventDefault();
+
+
+        navigate("/")
+    }
+
   return (
     <div className='min-h-[90vh]'>  
-        <div className='drawer absolute left-0 z-50 w-full'>
+        <div className='drawer absolute left-0 z-50 w-50'>
         <input id="my-drawer" type="checkbox" className="drawer-toggle" />
         <div className="drawer-content">
             <label htmlFor="my-drawer">
@@ -44,6 +61,12 @@ const HomeLayout = ({children}) => {
          <li>
             <Link to="/">Home</Link>
          </li>
+         {
+            isLoggedIn && role=="ADMIN" &&
+            (<li>
+                <Link to='/admin/dashboard'>Admin Dashboard</Link>
+            </li>)
+         }
          <li>
             <Link to="/about">About us</Link>
          </li>
@@ -53,6 +76,31 @@ const HomeLayout = ({children}) => {
          <li>
             <Link to="/courses">All Courses</Link>
          </li>
+
+         {
+            !isLoggedIn ? (<li className='absolute bottom-4 w-[90%]'>
+                
+                <div className='w-full flex items-center justify-center'>
+                    <button className='btn-primary px-4 py-1 font-semibold rounded-md w-full'>
+                        <Link to='/login'>Login</Link>
+                    </button>
+                    <button className='btn-secondary px-4 py-1 font-semibold rounded-md w-full'>
+                        <Link to='/signup'>SignUp</Link>
+                    </button>
+                </div>
+                 </li>
+                 ):   (<li className='absolute bottom-4 w-[90%]'>
+                
+                <div className='w-full flex items-center justify-center'>
+                    <button className='btn-primary px-4 py-1 font-semibold rounded-md'>
+                        <Link to='/user/profile'>Profile</Link>
+                    </button>
+                    <button className='btn-secondary px-4 py-1 font-semibold rounded-md'>
+                        <Link onClick={onLogout}>Logout</Link>
+                    </button>
+                </div>
+                 </li>)
+         }
         </ul>
         </div>
         </div>
