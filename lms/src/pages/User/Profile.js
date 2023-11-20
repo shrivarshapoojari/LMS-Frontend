c866f65a30362f8c5a48f476b7ed2815f4838a63
Shrivarsha <123290462+shrivarshapoojari@users.noreply.github.com>
2023-11-20 22:17:10 +0530

    Profile page

--- lms/src/pages/User/Profile.js
+++ lms/src/pages/User/Profile.js
@@ -1,16 +1,43 @@
 import React from 'react'
 import HomeLayout from '../../layouts/HomeLayout'
 import { useDispatch, useSelector } from 'react-redux'
-
+import { Link } from 'react-router-dom'
 const Profile = () => {
     const dispatch=useDispatch();
-    const userData=useSelector((state)=>state?.auth?.data)
+    const userData=useSelector((state)=>state?.auth?.data?.user)
+    
   return (
      <HomeLayout>
         <div className='min-h-[90vh] flex items-center justify-center' >
-            <div className='my-10 flex flex-col gap-4 rounded-lg p-4 text-white w-96 shadow-[0_0_100px_black]'>
+            <div className='my-10 flex flex-col gap-4 rounded-lg p-4 text-white w-96 shadow-[0_0_100px_purple]'>
+               <img src={userData?.avatar?.secure_url} alt="User" 
+               
+               className='w-40 m-auto rounded-full border border-purple-500 '/>
+                 <h3 className='text-xl font-semibold text-center capitalize'>
+              {userData?.fullname}
+            </h3>
+                  <div className='grid grid-cols-2'>
+                      <p>Email: </p> <p>{userData?.email}</p>
+                      <p>Role: </p> <p> {userData?.role}</p>
+                      <p>Subscription: </p> <p> {userData?.subscription?.status==="active"?"ACrive":"Expired"}</p>
+                       
+                  </div>
+                  
+                  <div className='flex items-center justify-between gap-2'>
+                     <Link to='/changepassword' className='w-1/2 bg-purple-600 hover:bg-purple-500 text-center px-2 py-2 rounded-sm font-semibold cursor-pointer'>
+                        Change Password
+                     </Link>
+                     <Link to='/user/editprofile' className='w-1/2 bg-purple-600 hover:bg-purple-500 text-center px-2 py-2 rounded-sm font-semibold cursor-pointer'>
+                        Edit Profile
+                     </Link>
+                  </div>
+                       {userData?.subscription?.status=='active' && (
+                       <button className=' bg-purple-600 hover:bg-purple-500 text-center px-2 py-2 rounded-sm font-semibold cursor-pointer'>
+                        Cancel Subscription
 
+                       </button>)} 
             </div>
+          
         </div>
      </HomeLayout>
   )
