72268382949398d3b2a0cf5b1e19508a272c8c5c
Shrivarsha <123290462+shrivarshapoojari@users.noreply.github.com>
2023-11-22 21:45:07 +0530

    UI changes

--- lms/src/layouts/Second.js
+++ lms/src/layouts/Second.js
@@ -1,10 +1,33 @@
 import React from 'react'
 import {useSelector } from 'react-redux/es/hooks/useSelector'
 import { BsPersonCircle } from 'react-icons/bs';
+import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { logout } from '../redux/slices/authSlice';
+import { useNavigate } from 'react-router-dom';
+
 const Second = () => {
     
 
+    const isLoggedIn=useSelector((state)=>state?.auth?.isLoggedIn)
+    const role=useSelector((state)=>state?.auth?.role);
     const userimg=useSelector(((state)=>state?.auth?.data?.avatar?.secure_url))
+    const dispatch=useDispatch()
+    const navigate=useNavigate()
+
+    async function  onLogout(e)
+    {
+        e.preventDefault();
+    
+
+      const response=  await dispatch(logout())
+      if(response?.payload?.data)
+      {
+        navigate("/")
+      }
+
+       
+    }
 
   return (
     <div className="navbar bg-base-100">
@@ -19,7 +42,7 @@ const Second = () => {
           <span className="badge badge-sm indicator-item">8</span>
         </div>
       </label>
-      <div tabIndex={0} className="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow">
+      <div tabIndex={0} className="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow-[0_0_100px_purple]">
         <div className="card-body">
           <span className="font-bold text-lg">8 Items</span>
           <span className="text-info">Subtotal: $999</span>
@@ -29,24 +52,42 @@ const Second = () => {
         </div>
       </div>
     </div>
-    <div className="dropdown dropdown-end">
+    <div className="dropdown dropdown-end ">
       <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-        <div className=" rounded-full flex items-center justify-center">
+        <div className=" rounded-full w-10">
             {
-                userimg?(<img src={userimg}/>):<BsPersonCircle/>
+                userimg?(<img src={userimg}/>):<BsPersonCircle className='h-[100%] w-[100%] object-cover'/>
             }
           
         </div>
       </label>
-      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2  bg-base-100 rounded-box w-52 shadow-[0_0_100px_purple]">
         <li>
-          <a className="justify-between">
-            Profile
-            <span className="badge">New</span>
-          </a>
+            
+          <Link to='/' className='justify-between text-white'>Home</Link>
         </li>
-        <li><a>Settings</a></li>
-        <li><a>Logout</a></li>
+        <li> <Link to='/about' className='justify-between text-white'>About Us</Link> </li>
+        <li> <Link to='/contact' className='justify-between text-white'>Contact Us</Link> </li>
+        <li><Link to='/courses' className='justify-between text-white'>Courses</Link></li>
+        {
+            isLoggedIn && role=="ADMIN" &&
+            (<li>
+                <Link to='/courses/create' className='justify-between text-white'>Add Course</Link>
+            </li>)
+            
+         }
+        {
+            !isLoggedIn ? ( <div>
+            <li><Link to='/login' className='justify-between text-white'>Login</Link></li>
+            <li><Link to='/signup' className='justify-between text-white'>Sign Up</Link></li> 
+            </div>):(<div>
+            <li><Link to='/user/profile' className='justify-between text-white'>Profile</Link></li>
+            <li><Link onClick={onLogout} className='justify-between text-white'>Logout</Link></li> 
+            </div>
+            )
+
+        }
+
       </ul>
     </div>
   </div>
